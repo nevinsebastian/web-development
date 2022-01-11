d3bc8b6de6daea3bd549272c081c85c716f6f30e
nevinsebastian <nevinsebastian.cse.carmelcet.in>
2022-01-12 01:32:14 +0530

    basic js added

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world';
+
+document.querySelector('html').onclick = function(){
+    alert('ouch! stop poking me!');
+}
