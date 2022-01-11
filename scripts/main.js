aa358915a04ca1d3fa9367f5920d0c401212db20
nevinsebastian <nevinsebastian.cse.carmelcet.in>
2022-01-12 02:06:43 +0530

    js addded to change image and to ask user name and a store it localstorage and print

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,30 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world';
+let myImage = document.querySelector('img');
 
-document.querySelector('html').onclick = function(){
-    alert('ouch! stop poking me!');
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc ==='images/firstim.jpg'){
+        myImage.setAttribute('src','images/secim.jpg');
+    }else{
+        myImage.setAttribute('src','images/firstim.jpg');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName(){
+    let myName = prompt('please eneter your name.');
+    localStorage.setItem('name',myName);
+    myHeading.textContent = 'WELCOME,'+ myName;
+}
+
+if(!localStorage.getItem('name')){
+    setUserName();
+}else{
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'WELCOME'+ storedName;
+}
+
+myButton.onclick = function(){
+    setUserName();
 }
